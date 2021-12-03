3cf55e47ac258f2e9f8c8e961693c3a93e0d48e7
DSPL001 <ds001pl@outlook.com>
2021-12-03 09:51:34 +0530

    RTY

--- portal/src/pages/authentication/login/index.js
+++ portal/src/pages/authentication/login/index.js
@@ -76,7 +76,7 @@ export default function SignIn() {
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
-              Sign In
+              Sign In..
             </Button>
             <Grid container>
               <Grid item xs>
